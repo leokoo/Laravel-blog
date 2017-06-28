5af8aab568ef42be1e57799c6545988c738c318a
Leo Koo <leokoo@gmail.com>
2017-06-29 00:23:11 +0800

    switched sidebar partial to master

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -20,9 +20,22 @@
 
   <body>
 		@include('layouts.nav')
+    <div class="container">
+      <div class="blog-header">
+        <h1 class="blog-title">The Bootstrap Blog</h1>
+        <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
+      </div>
 
-		@yield('content')
-    
+  <div class="row">
+    @yield('content')
+
+    @include('layouts.sidebar')
+
+  </div><!-- /.row -->
+
+</div><!-- /.container -->
+
+
 
     @include('layouts.footer')
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,16 +2,6 @@
 
 @section('content')
 
-
-<div class="container">
-
-  <div class="blog-header">
-    <h1 class="blog-title">The Bootstrap Blog</h1>
-    <p class="lead blog-description">The official example template of creating a blog with Bootstrap.</p>
-  </div>
-
-  <div class="row">
-
     <div class="col-sm-8 blog-main">
 
       <div class="blog-post">
@@ -81,13 +71,6 @@
       </nav>
 
     </div><!-- /.blog-main -->
-
-    @include('layouts.sidebar')
-
-
-  </div><!-- /.row -->
-
-</div><!-- /.container -->
 
 @endsection
 
