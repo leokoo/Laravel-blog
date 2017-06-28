c8c6012a6709f51d6ec7210f7bcc7c7daaeae1bc
Leo Koo <leokoo@gmail.com>
2017-06-28 13:20:25 +0800

    created show.blade.php to show individual posts as well as shifted master to layouts folder

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ Route::get('/tasks', 'TasksController@index');
 Route::get('/tasks/{task}', 'TasksController@show');
 
 Route::get('/', 'PostsController@index');
+Route::get('/posts/{post}', 'PostsController@show');
 
 Route::get('about', function () {
     return view('about');
