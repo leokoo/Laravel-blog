5d081a6a75824d9c0536a42e0c29d06f80ab346c
Leo Koo <leokoo@gmail.com>
2017-06-29 01:27:19 +0800

    finished design of content input form

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -4,8 +4,22 @@
 
 	<h1>Create a Post</h1>
 
-@endsection
+	<hr>
 
-@section('footer')
+	<form>
+
+	  <div class="form-group">
+	    <label for="title">Title:</label>
+	    <input type="text" class="form-control" id="title" name="title">
+	  </div>
+
+	  <div class="form-group">
+	    <label for="body">Body</label>
+	    <textarea name="body" id="body" class="form-control"></textarea>
+	  </div>
+
+	  <button type="submit" class="btn btn-primary ">Publish</button>
+
+	</form>
 
 @endsection
