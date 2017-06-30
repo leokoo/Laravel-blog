205afa843432d4154e5d9f8676727eff9d350122
Leo Koo <leokoo@gmail.com>
2017-07-01 01:19:13 +0800

    created server side validation

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -21,6 +21,20 @@
 	  </div>
 
 	  <button type="submit" class="btn btn-primary ">Publish</button>
+		
+		@if(count($errors))
+			<div class="form-group">
+				<div class="alert alert-danger">
+			  	<ul>
+			  		@foreach ($errors->all() as $error)
+			  			<li>
+			  				{{ $error }}
+			  			</li>
+			  		@endforeach
+			  	</ul>
+			  </div>
+			</div>
+		@endif
 
 	</form>
 
