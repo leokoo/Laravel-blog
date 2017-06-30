11452f73e713653bb75ad64a8344262641eb6de3
Leo Koo <leokoo@gmail.com>
2017-07-01 04:41:26 +0800

    show comments on individual posts

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,4 +7,26 @@
 
 	{{ $post->body }}
 
+	@if(count($post->comments))
+		<hr>
+		
+		<h3>Comments:</h3>
+	@endif
+
+	<div class="comments">
+		<ul class="list-group">
+				@foreach ($post->comments as $comment)
+				<li class="list-group-item">
+					<strong>
+						{{ $comment->created_at->diffForHumans() }}: &nbsp;	
+					</strong>
+					
+					{{ $comment->body }}
+				</li>
+				@endforeach
+		</ul>
+	</div>
+
+
+
 @endsection
