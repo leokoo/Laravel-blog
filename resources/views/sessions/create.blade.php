53f8d6e3357016c1c01593781e67d68809013ca7
Leo Koo <leokoo@gmail.com>
2017-07-02 22:51:15 +0800

    created the sessions.create blade file and form

---
+++ resources/views/sessions/create.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.master')
+
+@section('content')
+	<h1>Register</h1>
+
+	<form action="">
+		{{ csrf_field() }}
+
+		<div class="form-group">
+			<label for="name">Name:</label>
+			<input type="text" class="form-control" id="name" name="name">
+		</div>
+
+		<div class="form-group">
+			<label for="email">Email:</label>
+			<input type="email" class="form-control" id="email" name="email">
+		</div>
+
+		<div class="form-group">
+			<label for="password">Password:</label>
+			<input type="text" class="form-control" id="password" name="password">
+		</div>
+
+		<div class="form-group">
+			<button class="btn btn-primary" type="submit">Register</button>
+		</div>
+	</form>
+
+@endsection
