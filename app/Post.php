be6e54d4f32edd47a8102461565b03e16552e6e7
Leo Koo <leo.koo@mocento.com>
2017-09-09 14:46:53 +0800

    tags and pivot tables

--- app/Post.php
+++ app/Post.php
@@ -44,9 +44,14 @@ class Post extends Model {
 	public static function archives()
 	{
 		return static::selectRaw('year(created_at) year, monthname(created_at) month, count(*) published')
-            ->groupBy('year', 'month')
-            ->orderByRaw('min(created_at) desc')
-            ->get()
-            ->toArray();
+			->groupBy('year', 'month')
+			->orderByRaw('min(created_at) desc')
+			->get()
+			->toArray();
+	}
+
+	public function tags()
+	{
+		return $this->belongsToMany(Tag::class);
 	}
 }
